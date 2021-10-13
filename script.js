a8faabeb62611c565c42154f0cde3486e026be24
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-10-13 15:19:31 +0100

    functionality

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ timeline
     .fromTo('.box-1', {x:-300, y: -400 ,rotation: 0},{x:0, y: 0, rotation:360, duration: 5})
     .fromTo('.box-2', {x:300, y: -400 ,rotation: 0}, {x:0, y: 0, rotation:360,}, '-=5')
     .fromTo('.box-3', {x:450, y: 400 ,rotation: 0},{x:0, y: 0, rotation:360,},'-=5')
-    .fromTo('.box-4', {x:-450, y: 400 ,rotation: 0},{x:0, y: 0, rotation:360,}, '-=5');
-    // .to()
+    .fromTo('.box-4', {x:-450, y: 400 ,rotation: 0},{x:0, y: 0, rotation:360,}, '-=5')
+    .to('.')
 
     let scene = new ScrollMagic.Scene({
         triggerElement: '.front-cover',
